8053377239b267ecbb14e3e843fadac516a09f80
Jayden <jayden.cx.huang@gmail.com>
2021-09-14 19:50:16 -0700

    QOL Changes

--- guessinggame.cpp
+++ guessinggame.cpp
@@ -1,4 +1,5 @@
 //Setup
+#include <limits>
 #include <iostream>
 #include <stdlib.h> 
 #include <time.h>
@@ -13,10 +14,26 @@ int main() {
   srand (time(NULL));
   number = rand() % 100 + 1;
   int count = 0;
+  //Welcome Message
+  cout << "Hello! In this game a random number between 1 and 100 will be generated, each round you will have a chance to guess what that number is. The program will give you a hint after each guess. GLHF\n";
   //Main game loop
   while(running == true){
     cout << "What is your guess\n";
     cin >> guessnumber;
+    if(cin.fail()) {
+      cout << "Enter a valid input\n";
+      cin.clear();
+      cin.ignore(numeric_limits<streamsize>::max() , '\n');
+      cout << "What is your guess\n";
+      cin >> guessnumber;
+    }
+    if(guessnumber > 100) {
+      cout << "Enter a valid input\n";
+      cin.clear();
+      cin.ignore(numeric_limits<streamsize>::max() , '\n');
+      cout << "What is your guess\n";
+      cin >> guessnumber;
+    }
     //High Guess
     if(guessnumber > number){
       cout << "Your guess is too high!\n";
